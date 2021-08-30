ebf221df03bd6496581e1cc1a78ffa7d440c2ac7
mfrodrig <mfrodrig97@gmail.com>
2021-08-30 10:29:31 -0700

    ran flalsk app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
 def index():
     return "Under Constuction"
+
+if __name__ == "__main__":
+    app.run(debug = True)
